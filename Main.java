a3d1f507cb8331da2cd34f01fc546510717c406d
arslonbek22 <arslonbekyuldoshev01@gmail.com>
2024-01-15 19:46:22 +0500

    changes by arslonbek

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("salom");
         System.out.println("nima gap");
+        System.out.println("changes by arslonbek");
     }
 }
